515b4eaf05f62b6198807993e820b75d43e78ba1
Mike Lothar <mikelothar@gmail.com>
2017-11-05 22:41:43 +0100

    highlight active menu

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -1,16 +1,23 @@
 const m = require('mithril')
 
 module.exports = {
+  isActive(route) {
+    return m.route.get() === route ? 'active' : ''
+  },
   view(vnode) {
     return (
       <div>
         <nav className="fixed-top">
           <ul className="nav nav-pills nav-fill">
             <li className="nav-item">
-              <a className="nav-link" oncreate={m.route.link} href="/wines">Wines</a>
+              <a className={'nav-link ' + this.isActive('/wines')} 
+                oncreate={m.route.link}
+                href="/wines">Wines</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" oncreate={m.route.link} href="/add">Add</a>
+              <a className={'nav-link ' + this.isActive('/add')} 
+                oncreate={m.route.link} 
+                href="/add">Add</a>
             </li>
           </ul>
         </nav>
